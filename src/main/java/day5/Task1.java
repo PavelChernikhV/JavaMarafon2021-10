f7965572307765bf4d3e9c667cfb9a4d47886101
Pavel Chernikh <92542793+PavelChernikhV@users.noreply.github.com>
2021-10-17 17:55:43 +0500

    added day5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -10,6 +10,43 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-        
+
+        Car car1 = new Car();
+            car1.setModel("Жигули");
+            car1.setColor("Белый");
+            car1.setYear(1988);
+        System.out.println(car1.getModel()+" "+car1.getColor()+" "+car1.getYear());
+
+        Car car2 = new Car();
+            car2.setModel("Nexia");
+            car2.setColor("Бежевый");
+            car2.setYear(2000);
+        System.out.println(car2.getModel()+" "+car2.getColor()+" "+car2.getYear());
     }
+}
+
+class Car {
+    private String model;
+    private String color;
+    private int year;
+
+    public void setModel (String userModel){
+        model = userModel;
+    }
+    public String getModel(){
+        return model;
+    }
+    public void setColor(String userColor){
+        color = userColor;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setYear (int userYear){
+        year = userYear;
+    }
+    public int getYear(){
+        return year;
+    }
+
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -6,9 +6,28 @@ package day5;
     Вывести в консоль значение каждого из полей, используя get методы.
  */
 
-
 public class Task2 {
     public static void main(String[] args) {
-
+        Motorbike motorbike = new Motorbike("Чоппер", "Black", 2020);
+        System.out.println(motorbike.getModel() + " " + motorbike.getColor() + " " + motorbike.getYear());
     }
+}
+
+class Motorbike {
+    private String model;
+    private String color;
+    private int year;
+
+        public Motorbike(String model, String color, int year){
+            this.model = model;
+            this.color = color;
+            this.year = year;
+        }
+
+    public void setModel (String Model){this.model = Model;}
+    public String getModel(){return model;}
+    public void setColor(String Color){this.color = Color;}
+    public String getColor() {return color;}
+    public void setYear (int Year){this.year = Year;}
+    public int getYear(){return year;}
 }
